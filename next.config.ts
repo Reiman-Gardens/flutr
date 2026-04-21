bdc6a9bd836c96d6ad719ed536bcce36e951e6d9
Cai Chen <achen2304@gmail.com>
2026-04-20 21:51:57 -0500

    build: initilize docker containers

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     // Allows images to be loaded from the following domains - will be updated once the CS team finishes their project and makes a unified bucket.
     remotePatterns: [
